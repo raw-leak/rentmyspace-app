3314a22de18331be1a42bd22a615b042029ef783
unknown <michaelxxi@outlook.es>
2020-03-28 11:06:22 +0100

    add body

--- src/components/Body/index.js
+++ src/components/Body/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { BodySection } from "./styles";
-import List from "../../elements/List";
+import List from "../../elements/ListResult";
 import Map from "../../elements/Map";
 
 function Body() {
